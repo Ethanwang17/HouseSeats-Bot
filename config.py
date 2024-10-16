398566ca0bfd29356c1386fb430a70bae026be79
Ethanwang17 <ejw17@uw.edu>
2024-10-16 15:43:18 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# HouseSeats login credentials
+HOUSESEATS_EMAIL = "carolyn@wangusa.com"
+HOUSESEATS_PASSWORD = "HOw2rri0rz6"
+
+# Email configuration
+SENDER_EMAIL = "2ethanwang@gmail.com"
+SENDER_PASSWORD = "jipe pliv ncge iuxz"
+RECEIVER_EMAIL = "ethan@wangmail.site"
